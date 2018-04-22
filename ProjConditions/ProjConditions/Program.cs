0661b4b50287322f99a7b54ba9cdc2dc3bf840f6
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-22 17:45:54 +0200

    v

---
+++ ProjConditions/ProjConditions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjConditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "abc":
+                    Console.WriteLine("Pierwsze litery alfabetu.");
+                    break;
+                case "123":
+                    Console.WriteLine("Pierwsze cyfry.");
+                    break;
+                case "007":
+                    Console.WriteLine("James Bond");
+                    break;
+                case "olo":
+                    Console.WriteLine("Olo Technic");
+                    break;
+                default:
+                    Console.WriteLine("domyślne działanie");
+                    break;
+            }   
+            
+
+        }
+    }
+}
